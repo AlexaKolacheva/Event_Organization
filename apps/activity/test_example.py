6415cc21361e188f69dc8c1ba0dc5a067d5d1b1b
alexa <alexakola4eva@gmail.com>
2023-10-16 10:14:13 +0600

    final commit

--- apps/activity/test_example.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from .main import sum2
-
-def test_sum2():
-    assert sum2(15, 8) == 23
-
-# Create your tests here.
--- apps/events/permissions.py
+++ apps/events/permissions.py
@@ -19,7 +19,6 @@ class IsEventOwnerOrReadOnly(permissions.BasePermission):
 
 class IsEventCreatorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        # Разрешить POST запрос всем пользователям
         if request.method == 'GET':
             return True
         return request.user and request.user.is_authenticated
